4086f77b6ba8bead9c187c8d35cdc84ee28a25a2
LocalAdmin <LocalAdmin@CBE-ST305-25.towson.edu>
2020-04-28 14:52:40 -0400

    added CRUD features for backend

--- backend/routes/Users.js
+++ backend/routes/Users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 let User = require('../models/user.model');
 
 router.get('/', (req,res) => {
-    User.find()
+    User.find(req.params.id)
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err));
 });
@@ -23,5 +23,32 @@ newUser.save()
 res.status(400).json('Error: ' + err);
 });
 });
+router.get('/:id', (req,res) => {
+User.findById(req.params.id)
+.then(users => res.json(users))
+.catch(err => res.status(400).json('Error: ' + err));
+});
+router.delete('/:id', (req,res) => {
+User.findByIdAndDelete(req.params.id)
+.then(() => res.json('Applicant Deleted.'))
+.catch(err => res.status(400).json('error: ' + err));
+});
+router.post('/update:id', (req, res) => {
+User.findById(req.params.id)
+.then(User => {
+    User.username = req.body.username;
+    User.loanrequest = Number(req.body.loanrequest);
+    address = req.params.address;
 
+    User.save()
+    .then(data => {
+        res.json(data);
+    })
+    
+    .catch(err => {
+        res.status(400).json('Error: ' + err);
+});
+});
+});
+ 
 module.exports = router;
